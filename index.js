398126aab1b143916f1612d69031a45d87932222
Josh Brackett <Wbrackett85@gmail.com>
2018-09-20 20:07:45 -0600

    tv-demo part 11

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
-app.get('/tv-demo', (req, res) => res.send('Enjoy your show!'))
 
-app.post(`/tv-demo`, (req, res) => res.send(`You have selected your show`))
+      const tvShows = []
+        
+ 
+
+app.get('/tv-demo', (req, res) => {
+    res.send(tvShows)
+})
+
+app.post('/tv-demo', (req, res) =>  {
+    tvShows.push(req.body)
+    res.send(tvShows)
+})
 
 app.put('/tv-demo', (req, res) => res.send('You have changed your selection'))
 
 app.delete('/tv-demo', (req, res) => res.send('Your show was cancelled'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(` app listening on port ${port}!`))
