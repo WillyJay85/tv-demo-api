044c42076f04a1225f405e625df44ca1f653d87b
Josh Brackett <Wbrackett85@gmail.com>
2018-09-24 18:12:30 -0600

    tv-demo-11

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 
       const tvShows = []
